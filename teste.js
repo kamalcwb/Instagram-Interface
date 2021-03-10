f0d5f1d6d62033c4304738d5013e13f40cf00247
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-03-09 23:27:25 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,13 @@
+// complete as variaveis necessarias
+var n = parseInt();
+var kg = parseFloat()
+
+while (n > 0) { //complete o codigo
+    var c = parseFloat();
+    let dias = n; //atribua o valor correto a variavel
+    while (kg >= 1) { // complete o while
+        let kg = kg / 2
+        let dias = dias++
+    }
+    console.log(dias` dias`); //complete o código para a saída
+}
